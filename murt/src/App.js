3d9309b7647ea4bd9dbfd031a9b4cab6d41dfbb6
lwod <lwodcurrentmail@gmail.com>
2021-04-02 06:53:20 +0300

    checkbox basic

--- murt/src/App.js
+++ murt/src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import DeleteIcon from '@material-ui/icons/Delete'
+
+import Checkbox from '@material-ui/core/Checkbox'
+
+function CheckboxExample(){
+    const [checked, setChecked] = useState(true)
+    return(
+        <div>
+            <Checkbox
+                checked={checked}
+                onChange={(e)=>setChecked(e.target.checked)}
+                color={'secondary'}
+                // disabled={true}
+            />
+        </div>
+    )
+}
 
 function App() {
   return (
@@ -24,8 +41,10 @@ function App() {
             >
                 Discard
             </Button>
-
+            
         </ButtonGroup>
+        
+        <CheckboxExample/>
         
         
       </header>
